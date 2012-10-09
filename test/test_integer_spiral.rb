94b043a6c974a9e80f38bbb3d4bbba060fbff615
Michael Dellanoce <michael.dellanoce@gmail.com>
2012-10-09 00:18:24 -0500

    non-square cases

--- test/test_integer_spiral.rb
+++ test/test_integer_spiral.rb
@@ -11,11 +11,30 @@ class TestIntegerSpiral < Test::Unit::TestCase
     )
   end
 
+  def test_one
+    spiral = IntegerSpiral.new 1
+    assert_equal(
+      [[  0,  1],
+       [nil,nil]],
+      spiral.to_a
+    )
+  end
+
   def test_three
     spiral = IntegerSpiral.new 3
     assert_equal(
       [[0,1],
        [3,2]],
+      spiral.to_a
+    )
+  end
+
+  def test_four
+    spiral = IntegerSpiral.new 4
+    assert_equal(
+      [[nil,nil,nil],
+       [nil,  0,  1],
+       [  4,  3,  2]],
       spiral.to_a
     )
   end
@@ -26,6 +45,17 @@ class TestIntegerSpiral < Test::Unit::TestCase
       [[6,7,8],
        [5,0,1],
        [4,3,2]],
+      spiral.to_a
+    )
+  end
+
+  def test_nine
+    spiral = IntegerSpiral.new 9
+    assert_equal(
+      [[  6,  7,  8,  9],
+       [  5,  0,  1,nil],
+       [  4,  3,  2,nil],
+       [nil,nil,nil,nil]],
       spiral.to_a
     )
   end
@@ -41,7 +71,19 @@ class TestIntegerSpiral < Test::Unit::TestCase
     )
   end
 
-  def test_fifteen
+  def test_sixteen
+    spiral = IntegerSpiral.new 16
+    assert_equal(
+      [[nil,nil,nil,nil,nil],
+       [nil,  6,  7,  8,  9],
+       [nil,  5,  0,  1, 10],
+       [nil,  4,  3,  2, 11],
+       [ 16, 15, 14, 13, 12]],
+      spiral.to_a
+    )
+  end
+
+  def test_twentyfour
     spiral = IntegerSpiral.new 24
     assert_equal(
       [[20,21,22,23,24],
